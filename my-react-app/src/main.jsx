0cb3c772272c6ee7071c2e25d1d7f4b90b48588b
Nitish <snkumar6926@gmail.com>
2025-12-26 12:19:43 +0530

    The Html Code rendering

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -6,6 +6,25 @@ import { createRoot } from 'react-dom/client'
 )*/
 
 // Example of render method...
-createRoot(document.getElementById('root')).render(
+/*createRoot(document.getElementById('root')).render(
   <p>Welcome!</p>
+)*/
+
+// Example of Html Code rendering...
+const myelement = (
+  <table>
+    <tr>
+      <th>Name</th>
+    </tr>
+    <tr>
+      <th>Nitish</th>
+    </tr>
+    <tr>
+      <th>Singh</th>
+    </tr>
+  </table>
+)
+
+createRoot(document.getElementById('root')).render(
+  myelement
 )
