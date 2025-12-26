ed7af1ec9eff838172446d1ea3322861d371ab36
Nitish <snkumar6926@gmail.com>
2025-12-26 12:04:48 +0530

    The render method

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,5 +1,11 @@
 import { createRoot } from 'react-dom/client'
 
-createRoot(document.getElementById('root')).render(
+// Example of rendering with createRoot function...
+/*createRoot(document.getElementById('root')).render(
   <h1> Hello World </h1>
+)*/
+
+// Example of render method...
+createRoot(document.getElementById('root')).render(
+  <p>Welcome!</p>
 )
