73536908f52c82bea5310ec2c37a698c91b1d8e2
Nitish <snkumar6926@gmail.com>
2026-01-09 21:32:47 +0530

    Example of conditions - if statements

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -336,7 +336,7 @@ createRoot(document.getElementById('root')).render(
 );*/
 
 // Example of React JSX IF Statement
-function Fruit() {
+/*function Fruit() {
   const x = 5;
   let y = "Apple";
   if (x < 10) {
@@ -345,6 +345,17 @@ function Fruit() {
 
   return (
     <h1>{y}</h1>
+  );
+}
+
+createRoot(document.getElementById('root')).render(
+  <Fruit />
+);*/
+
+function Fruit() {
+  const x = 5;
+  return (
+    <h1>{(x) < 10 ? "Banana" : "Apple"}</h1>
   );
 }
 
