9e56481e1e83ecf57fe8684e2870ebc85b771653
Nitish <snkumar6926@gmail.com>
2026-01-12 00:59:25 +0530

    Example of using of state object

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -527,6 +527,35 @@ createRoot(document.getElementById('root')).render(
 );*/
 
 // Example of Components in Files.
-createRoot(document.getElementById('root')).render(
+/*createRoot(document.getElementById('root')).render(
   <Car />
-);
+);*/
+
+// Example of Using of state Object
+class Car extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brand: "Ford",
+      model: "Mustang",
+      color: "red",
+      year: 1964
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>My {this.state.brand}</h1>
+        <p>
+          It is a {this.state.color}
+          {this.state.model}
+          from {this.state.year}.
+        </p>
+      </div>
+    );
+  }
+}
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<Car />);
