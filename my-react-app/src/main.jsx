049c4f97dc29a76ce892f02e57e27d5bcc24a779
Nitish <snkumar6926@gmail.com>
2026-01-13 14:41:48 +0530

    Example of Changing the state Object

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -532,7 +532,7 @@ createRoot(document.getElementById('root')).render(
 );*/
 
 // Example of Using of state Object
-class Car extends React.Component {
+/*class Car extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -558,4 +558,40 @@ class Car extends React.Component {
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-root.render(<Car />);
+root.render(<Car />);*/
+
+// Example of Changing the state Object
+class Car extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brand: "Ford",
+      model: "Mustang",
+      color: "red",
+      year: 1964
+    };
+  }
+  changeColor = () => {
+    this.setState({color: "blue"});
+  }
+  render() {
+    return (
+      <div>
+        <h1>My {this.state.brand}</h1>
+        <p>
+          It is a {this.state.color}
+          {this.state.model}
+          from {this.state.year}.
+        </p>
+        <button
+          type="button"
+          onClick={this.changeColor}
+        >Change color</button>
+      </div>
+    );
+  }
+}
+
+createRoot(document.getElementById('root')).render(
+  <Car />
+)
