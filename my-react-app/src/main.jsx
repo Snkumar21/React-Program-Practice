0b8369a0144c54ad8325380296e2e16f3f7d78d7
Nitish <snkumar6926@gmail.com>
2026-01-28 00:52:47 +0530

    Example of React Forms - Radio

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1545,7 +1545,7 @@ createRoot(document.getElementById('root')).render(
 );*/
 
 // Example of Use initial values for the input fields.
-function MyForm() {
+/*function MyForm() {
   const [inputs, setInputs] = useState({
     firstname: 'John',
     tomato: true,
@@ -1602,6 +1602,58 @@ function MyForm() {
         <button type="submit">Submit</button>
     </form>
   )
+}
+
+createRoot(document.getElementById('root')).render(
+  <MyForm />
+);*/
+
+// Example of React Forms - Radio
+function MyForm() {
+  const [selectedFruit, setSelectedFruit] = useState('banana');
+
+  const handleChange = (event) => {
+    setSelectedFruit(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    alert(`Your favorite fruit is: ${selectedFruit}`);
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>Select your favorite fruit:</p>
+      <label>
+        <input 
+          type="radio" 
+          name="fruit" 
+          value="apple" 
+          checked={selectedFruit === 'apple'} 
+          onChange={handleChange} /> Apple
+      </label>
+      <br />
+      <label>
+        <input 
+          type="radio" 
+          name="fruit" 
+          value="banana" 
+          checked={selectedFruit === 'banana'} 
+          onChange={handleChange} /> Banana
+      </label>
+      <br />
+      <label>
+        <input 
+          type="radio" 
+          name="fruit" 
+          value="cherry" 
+          checked={selectedFruit === 'cherry'} 
+          onChange={handleChange} /> Cherry
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 createRoot(document.getElementById('root')).render(
