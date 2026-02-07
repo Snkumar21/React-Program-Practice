4e8decf6be0f042dd58043200186b4c0010ef396
Nitish <snkumar6926@gmail.com>
2026-02-07 22:41:27 +0530

    Example of React useMemo Hook

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -2954,8 +2954,8 @@ createRoot(document.getElementById('root')).render(
   <WithCallbackExample />
 );*/
 
-// Example of React without useMemo Hook
-const App = () => {
+// Example of React without useMemo Hook.
+/*const App = () => {
   const [count, setCount] = useState(0);
   const [todos, setTodos] = useState([]);
   const calculation = expensiveCalculation(count);
@@ -2983,6 +2983,52 @@ const App = () => {
         <h2>Expensive Calculation</h2>
         {calculation}
         <p>Note that this example executes the expensive function also when you click on the Add Todo button.</p>
+      </div>
+    </div>
+  );
+};
+
+const expensiveCalculation = (num) => {
+  console.log("Calculating...");
+  for (let i = 0; i < 1000000000; i++) {
+    num += 1;
+  }
+  return num;
+};
+
+createRoot(document.getElementById('root')).render(
+  <App />
+);*/
+
+// Example of React useMemo Hook.
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+  const calculation = useMemo(() => expensiveCalculation(count), [count]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+  const addTodo = () => {
+    setTodos((t) => [...t, "New Todo"]);
+  };
+
+  return (
+    <div>
+      <div>
+        <h2>My Todos</h2>
+        {todos.map((todo, index) => {
+          return <p key={index}>{todo}</p>;
+        })}
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+        <h2>Expensive Calculation</h2>
+        {calculation}
+        <p>Note: This example executes the expensive function only when you click on the Increment button, and not when you add a todo.</p>
       </div>
     </div>
   );
